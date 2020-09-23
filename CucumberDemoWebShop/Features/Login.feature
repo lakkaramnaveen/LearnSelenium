a3c1618a11aced2c17486ffcff7436a752949355
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2020-09-23 17:25:52 +0530

    Day 3 end

--- CucumberDemoWebShop/Features/Login.feature
+++ CucumberDemoWebShop/Features/Login.feature
@@ -5,3 +5,9 @@ Given User in on login page
 When User enters valid username and password
 And User clicks on login button
 Then User should be able to see Logout button.
+
+Scenario: UnSuccessful login with valid credentials
+Given User has opened login page
+When User enters valid username and invalid password
+And User clicked on login button
+Then User should be able to see "Login was unsuccessfull"
--- CucumberDemoWebShop/src/stepDefinition/InvalidLogin.java
+++ CucumberDemoWebShop/src/stepDefinition/InvalidLogin.java
@@ -12,11 +12,11 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class CheckLogin {
+public class InvalidLogin {
 	WebDriver driver;
-	
-	@Given("^User in on login page$")
-	public void user_in_on_login_page() {
+
+	@Given("^User has opened login page$")
+	public void user_has_opened_login_page() throws Throwable {
 		System.setProperty("webdriver.chrome.driver", "D:\\SeleniumCucumberSoftware\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://demowebshop.tricentis.com/login"); //open the website from url
@@ -25,25 +25,24 @@ public class CheckLogin {
 		String actual_title = driver.getTitle();
 		System.out.println(actual_title);
 		assertEquals(expected_title,actual_title);
-		//assert driver.getTitle().contains("Demo Web Shop");
-		
-	}
-	@When("^User enters valid username and password$")
-	public void user_enters_valid_username_and_password() throws Throwable {
-		driver.findElement(By.id("Email")).sendKeys("naveen1234@gmail.com");
-		driver.findElement(By.name("Password")).sendKeys("123456");
 	}
 
-	@And("^User clicks on login button$")
-	public void user_clicks_on_login_button() throws Throwable {
+	@When("^User enters valid username and invalid password$")
+	public void user_enters_valid_username_and_invalid_password() throws Throwable {
+		driver.findElement(By.id("Email")).sendKeys("naveen1234@gmail.com");
+		driver.findElement(By.name("Password")).sendKeys("1234");
+	}
+
+	@And("^User clicked on login button$")
+	public void user_clicked_on_login_button() throws Throwable {
 		driver.findElement(By.xpath("//input[@class='button-1 login-button']")).click();
 	}
 
-	@Then("^User should be able to see Logout button\\.$")
-	public void user_should_be_able_to_see_Logout_button() throws Throwable {
-		boolean view = driver.findElement(By.linkText("Log out")).isEnabled();
+	@Then("^User should be able to see \"([^\"]*)\"$")
+	public void user_should_be_able_to_see(String arg1) throws Throwable {
+		boolean view = driver.findElement(By.xpath("//span[contains(text(),'Login was unsuccessful. Please correct the errors')]")).isDisplayed();
 		assertTrue(view);
-		driver.findElement(By.linkText("Log out")).click();	
+		driver.findElement(By.xpath("//span[contains(text(),'Login was unsuccessful. Please correct the errors')]"));
 		driver.close();
 	}
 }
